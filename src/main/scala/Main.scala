10c02f5d2a1a1dec836557d90b285c626b175f3f
Elar Saks <elarsaks@gmail.com>
2024-04-29 10:29:38 +0300

    Read data from CSV.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,19 @@
 package sparkvideocourse
 
+import org.apache.spark.sql.SparkSession
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    val spark = SparkSession.builder()
+      .appName("Spark-video-course")
+      .config("spark.driver.bindAddress", "127.0.0.1")
+      .master("local[*]")
+      .getOrCreate()
+
+    val df = spark.read
+      .option("header", value = true)
+      .csv("data/AAPL.csv")
+
+    df.show()
   }
 }
